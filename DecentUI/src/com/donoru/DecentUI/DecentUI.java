dcaa57cfed4c195014091830f526bbfb47c8dcef
d0noru <donorusr@gmail.com>
2024-02-12 17:22:25 -0700

    Created project file hierarchy

--- DecentUI/src/com/donoru/DecentUI/DecentUI.java
+++ DecentUI/src/com/donoru/DecentUI/DecentUI.java
@@ -1,6 +1,5 @@
 package com.donoru.DecentUI;
 
-import com.donoru.DecentUI.Commands.Commands;
 import com.donoru.DecentUI.Events.Events;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,14 +8,13 @@ public class DecentUI extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Commands commands = new Commands();
-        getCommand("lobby").setExecutor(new Commands());
         getServer().getPluginManager().registerEvents(new Events(), this);
+        getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "[DecentUI]: Plugin is enabled");
     }
 
     @Override
     public void onDisable() {
-        getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "[Bedwars]: Plugin is disabled");
+        getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "[DecentUI]: Plugin is disabled");
     }
 
 }
