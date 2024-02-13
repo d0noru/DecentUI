a17ca021eebff210178c8ce3b5a7e4f61f17da5d
d0noru <donorusr@gmail.com>
2024-02-13 16:38:15 -0700

    create.java

---
+++ DecentUI/src/com/donoru/DecentUI/Commands/create.java
@@ -0,0 +1,45 @@
+package com.donoru.DecentUI.Commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import com.donoru.DecentUI.DecentUI.*;
+
+import java.io.File;
+
+public class create implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!(sender instanceof Player)) {
+			return true;
+		}
+
+		Player player = (Player) sender;
+
+		if (cmd.getName().equalsIgnoreCase("dui")) {
+			if (args.length > 0) {
+				if (args[0].equals("create")) {
+					if (args.length == 3) {
+						if (args[2].matches("[0-9]+") && Integer.parseInt(args[2]) <= 6) {
+							player.sendMessage("UI '" + args[1] + "' created with " + args[2] + " rows.");
+
+							File config = new File("", args[1] + ".yml");
+
+						} else {
+							player.sendMessage("Rows must be a number 6 or lower.");
+						}
+					} else {
+						player.sendMessage("Usage: /dui create <name> <rows>");
+					}
+				}
+			}
+		}
+
+		return true;
+	}
+
+}
--- DecentUI/src/com/donoru/DecentUI/DecentUI.java
+++ DecentUI/src/com/donoru/DecentUI/DecentUI.java
@@ -1,13 +1,18 @@
 package com.donoru.DecentUI;
 
+import com.donoru.DecentUI.Commands.create;
 import com.donoru.DecentUI.Events.Events;
 import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public class DecentUI extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getCommand("dui").setExecutor(new create());
         getServer().getPluginManager().registerEvents(new Events(), this);
         getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "[DecentUI]: Plugin is enabled");
     }
@@ -15,6 +20,18 @@ public class DecentUI extends JavaPlugin {
     @Override
     public void onDisable() {
         getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "[DecentUI]: Plugin is disabled");
+    }
+
+    public void plugin() {
+        Plugin plugin;
+    }
+
+    public void createConfig(String name) {
+        File config = new File(getDataFolder(), name = ".yml");
+
+        if (!config.exists()) {
+
+        }
     }
 
 }
